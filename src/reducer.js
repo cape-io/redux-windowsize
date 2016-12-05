89dd6fbdeeea7a5dbb349f1eae3bedf509fe7ee1
Kai Curry <kai@cape.io>
2016-12-04 22:59:30 -0600

    Add max val reducer for height/width

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,34 @@
+import { eq } from 'lodash'
 import { createReducer } from 'cape-redux'
-import { merge, setKey } from 'cape-lodash'
+import { setKey } from 'cape-lodash'
 import { SET_HEIGHT, SET_ID, SET_REM, SET_SIZE, SET_WIDTH } from './actions'
 
 export const defaultState = {
   height: null,
+  heightMax: null,
   id: null,
   rem: null,
   width: null,
+  widthMax: null,
 }
+export function setDimension(id) {
+  const maxKey = `${id}Max`
+  return (state, payload) => {
+    if (eq(state[id], payload)) return state
+    return { ...state, [id]: payload, [maxKey]: Math.max(state[maxKey] || 0, payload) }
+  }
+}
+export const heightReducer = setDimension('height')
+export const widthReducer = setDimension('width')
+export const sizeReducer = (state, { width, height }) => heightReducer(
+  widthReducer(state, width), height
+)
 
 export const reducers = {
-  [SET_HEIGHT]: setKey('height'),
+  [SET_HEIGHT]: heightReducer,
   [SET_ID]: setKey('id'),
   [SET_REM]: setKey('rem'),
-  [SET_SIZE]: merge,
-  [SET_WIDTH]: setKey('width'),
+  [SET_SIZE]: sizeReducer,
+  [SET_WIDTH]: widthReducer,
 }
 export default createReducer(reducers, defaultState)
