51fa6f1a8c60bd0b31bf8a360fcfa4ba67aeb4cb
Kai Curry <kai@cape.io>
2016-12-04 23:00:02 -0600

    test maxval settings

--- test/reducer.spec.js
+++ test/reducer.spec.js
@@ -1,6 +1,21 @@
 import test from 'tape'
+import { isFunction } from 'lodash'
 import reducer, { setHeight, setId, setRem, setSize, setWidth } from '../src'
+import { defaultState, setDimension } from '../src/reducer'
 
+test('setDimension', (t) => {
+  const func = setDimension('rem')
+  t.ok(isFunction(func))
+  t.equal(func(defaultState, null), defaultState)
+  const res = func(defaultState, 10)
+  t.equal(res.rem, 10)
+  t.equal(res.remMax, 10)
+  t.equal(func(res, 10), res)
+  const res2 = func(res, 8)
+  t.equal(res2.rem, 8)
+  t.equal(res2.remMax, 10)
+  t.end()
+})
 test('setHeight', (t) => {
   t.equal(reducer(undefined, setHeight('10')).height, 10)
   t.end()
@@ -17,6 +32,7 @@ test('setSize', (t) => {
   const state = reducer(undefined, setSize(800, 900))
   t.equal(state.height, 800)
   t.equal(state.width, 900)
+  t.equal(reducer(state, setSize(800, 900)), state)
   t.end()
 })
 test('setWidth', (t) => {
